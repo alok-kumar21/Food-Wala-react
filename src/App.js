e1473700de6273c92bb952558d1985dc374186c1
alok-kumar21 <alokprajapati021@gmail.com>
2025-04-12 09:16:34 +0530

    Food delivery app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  const img1 =
+    "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D";
+  const img2 =
+    "https://plus.unsplash.com/premium_photo-1661883237884-263e8de8869b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJlc3RhdXJhbnR8ZW58MHx8MHx8fDA%3D";
+  return (
+    <main className="container py-4">
+      <h1 className="fw-normal">Featured Restaurants</h1>
+      <div className="mt-3 ">
+        <img src={img1} alt="img" className="img-fluid rounded" />
+        <h2 className="mt-2">Taste of Italy</h2>
+        <p>Italian | Rating 4.5</p>
+        <p>123 Main Street, Anytwon, USA</p>
+      </div>
+      <div className="mt-3 ">
+        <img src={img2} alt="img" className="img-fluid rounded" />
+        <h2 className="mt-2">Spice Village</h2>
+        <p>Indian | Rating 4.3</p>
+        <p>456 Elm Street, Anytwon, India</p>
+      </div>
+    </main>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import About from "./pages/About";
+import Restaurants from "./pages/Restaurants";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/restaurants",
+    element: <Restaurants />,
+  },
+]);
+
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
